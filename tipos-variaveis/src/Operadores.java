ccecbc033e1b53aef74038d9b48b8e9ff255c315
Mateus Greco <mat066397@gmail.com>
2024-07-17 18:29:31 -0300

    Criação de arquivos simples, iniciais/complementares, e pequeno exemplo de fixação de conteúdo.

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,50 @@
+// Arquivo .java apenas com anotações dos tipos de operadores, leve sintaxe, regras, etc;
+public class Operadores {
+    public static void main(String[] args) {
+        /*
+        boolean condicao1 = true;
+
+        boolean condicao2 = true;
+
+        if(condicao1 && condicao2){
+            System.out.println("As duas Verdadeiras");
+        }
+
+        if(condicao1 && (7 > 4)){
+            System.out.println("As duas Verdadeiras");
+        }
+
+        if(condicao1 || condicao2){
+            System.out.println("uma delas Verdadeira");
+        }
+
+        /////////////////////////////////////////////////////////////////////////////////////////
+
+        String nomeUm = "TESTE";
+        String nomeDois = new String("TESTE");
+
+        System.out.println(nomeUm.equals(nomeDois));
+
+        
+        
+        int numero1 = 1;
+        int numero2 = 2;
+
+        boolean simNao = numero1 == numero2;
+
+        if(numero1 < numero2){
+            System.out.println("Menor? " + simNao);
+        }
+
+        System.out.println("Igual? " + simNao);
+
+        simNao = numero1 != numero2;
+
+        System.out.println("Diferente? " + simNao);
+
+        simNao = numero1 > numero2;
+
+        System.out.println("Maior? " + simNao);
+        */
+    }
+}
--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,22 @@
+// Arquivo .java apenas com anotações dos tipos de variáveis, leve sintaxe, regras, etc;
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        /*
+        String meuNome = "TESTE TESTE";
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        System.out.println(numeroCurto2);
+
+        int numero = 5;
+
+        numero = 10;
+
+        System.out.println(numero);
+
+        final double VALOR_DE_PI = 3.14;
+        */
     }
 }
